4462d56af7ae449dfe17aa909e2de8c62981eac7
ankittkamal <officiallyankit999@gmail.com>
2023-09-10 23:56:35 +0530

    Added idempotency key to make payment

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 
 const { ServerConfig, Logger } = require("./config");
-
 const apiRoutes = require("./routes");
+const CRON = require("./utils/common/cron-jobs");
 
 const app = express();
 
@@ -14,4 +14,5 @@ app.use("/api", apiRoutes);
 app.listen(ServerConfig.PORT, () => {
   console.log(`Successfully started the server on PORT: ${ServerConfig.PORT}`);
   Logger.info("Successfully started the server", "root", {});
+  CRON();
 });
--- src/utils/common/index.js
+++ src/utils/common/index.js
@@ -2,4 +2,5 @@ module.exports = {
   ErrorResponse: require("./errror-response"),
   SuccessResponse: require("./success-response"),
   Enums: require("./enums"),
+  CRONS: require("./cron-jobs"),
 };
